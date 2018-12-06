3ac524f7c782ae7bbbb3338b1504614265871a1c
Kevin Vosburgh <kvosbur@purdue.edu>
2018-12-06 18:36:22 -0500

    Set up example where you set accept type and be able to return a json object.

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,7 +1,27 @@
 import static spark.Spark.*;
 
 public class Server {
-    public static void main(String[] args) {
+
+    public Server(){
+        //change port for server (default is 4567)
+        //port(20000)
+
+        endpoints();
+
+
+    }
+
+    /**
+     Use this method to add all endpoints that server is to be hosting
+     */
+    private void endpoints(){
         get("/hello", (req, res) -> "Hello world");
+        get("/index", "application/json", (request, response) -> {
+            return "text";
+        }, new JsonTransformer());
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
     }
 }
